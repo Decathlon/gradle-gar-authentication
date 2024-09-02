c16290870b9d400e322d06b662adba897d014232
Vincent Masselis <vincent.masselis.partner@decathlon.com>
2024-09-02 10:30:21 +0200

    Fixed inline bytecode conflict with users

--- src/main/kotlin/com/decathlon/android/gradle/authenticatedgarcli/GarCredentials.kt
+++ src/main/kotlin/com/decathlon/android/gradle/authenticatedgarcli/GarCredentials.kt
@@ -4,6 +4,5 @@ import org.gradle.api.artifacts.repositories.AuthenticationSupported
 import org.gradle.api.plugins.ExtensionAware
 import org.gradle.kotlin.dsl.the
 
-@Suppress("NOTHING_TO_INLINE")
-public inline fun ExtensionAware.garCliCredentials(parent: AuthenticationSupported): Unit =
+public fun ExtensionAware.garCliCredentials(parent: AuthenticationSupported): Unit =
     the<AuthenticatedGarCliExtension>().garCliCredentials(parent)
