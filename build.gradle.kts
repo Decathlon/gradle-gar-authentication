c8d542827b2881515abb3d2b41e447117f512ce5
GerardPaligot <gerard.paligot@gmail.com>
2024-10-08 11:08:09 +0200

    chore: update version to 0.1.0-alpha02.

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 }
 
 group = "com.decathlon.gradle"
-version = "0.1.0-alpha01"
+version = "0.1.0-alpha02"
 val releaseVersion = version.toString().endsWith("-SNAPSHOT").not()
 
 java {
