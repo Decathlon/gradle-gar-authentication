94bf1a6d800f05258fa241d3fc044d947b64698a
Adrien BUSIN <adrien.busin@decathlon.com>
2025-07-21 10:03:26 +0200

    chore: update version to 0.1.0-alpha03.

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 }
 
 group = "com.decathlon.gradle"
-version = "0.1.0-alpha02"
+version = "0.1.0-alpha03"
 val releaseVersion = version.toString().endsWith("-SNAPSHOT").not()
 
 java {
