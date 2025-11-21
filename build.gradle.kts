5a043ab33be29a4e3c7721655bd891b24c990d0e
Vincent Masselis <vincent.masselis@gmail.com>
2025-11-21 10:58:59 +0100

    chore: update version to 0.1.0-alpha07

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 }
 
 group = "com.decathlon.gradle"
-version = "0.1.0-alpha06"
+version = "0.1.0-alpha07"
 val releaseVersion = version.toString().endsWith("-SNAPSHOT").not()
 
 java {
